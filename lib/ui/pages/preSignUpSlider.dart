8ef6180f5b3d7524cbcf6062032cb726cb81b571
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-06-08 20:46:35 +0530

    PreSignUpSlider done

--- lib/ui/pages/preSignUpSlider.dart
+++ lib/ui/pages/preSignUpSlider.dart
@@ -1,9 +1,150 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PreSignUpSlider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: height,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 50),
+                child: CarouselSlider(
+                  items: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Container(
+                        child: Image.asset(
+                          'assets/images/preSignUpSlider1.png',
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Container(
+                        child: Image.asset(
+                          'assets/images/preSignUpSlider2.png',
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      child: Container(
+                        child: Image.asset(
+                          'assets/images/preSignUpSlider3.png',
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                  ],
+                  options: CarouselOptions(
+                    height: height * 0.5,
+                    viewportFraction: 1,
+                    enlargeCenterPage: true,
+                    autoPlay: true,
+                    autoPlayCurve: Curves.easeInOutExpo,
+                  ),
+                ),
+              ),
+              Text(
+                'Eco-friendly',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 50,
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xFF000000),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 60.0, vertical: 15),
+                child: Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF000000),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(40, 60, 40, 0),
+                child: FlatButton(
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  color: Color(0xFF004445),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          'Sign Up',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xFFFFFFFF),
+                          ),
+                        ),
+                      ),
+                      Image.asset(
+                        'assets/icons/arrow.png',
+                        width: 20,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(40, 0, 40, 0),
+                child: FlatButton(
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(0.0),
+                          child: Text(
+                            'Already have an account?  ',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontFamily: 'Roboto',
+                              fontSize: 17,
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xFF000000),
+                            ),
+                          ),
+                        ),
+                        Text(
+                          'Login',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 17,
+                            fontWeight: FontWeight.w900,
+                            color: Color(0xFF000000),
+                          ),
+                        ),
+                      ],
+                    )),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
