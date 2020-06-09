87e3ba44f9168da6b96e3bfc65793686449311d6
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-06-09 15:03:45 +0530

    Added strapi server

---
+++ server/config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', '@shoppy-mwqei.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'server'),
+        username: env('DATABASE_USERNAME', 'hashStudios'),
+        password: env('DATABASE_PASSWORD', 'Hash*Studios'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
