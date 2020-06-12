4b7eb9fd2753b15801b569f014e22b98fe82c003
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-06-12 17:12:12 +0530

    Updated profile page

--- lib/ui/pages/profileScreen.dart
+++ lib/ui/pages/profileScreen.dart
@@ -1,8 +1,11 @@
+import 'package:eco_commerce_app/core/data/sharedPrefHandler.dart';
+import 'package:eco_commerce_app/routing_constants.dart';
 import 'package:eco_commerce_app/ui/widgets/productListTile.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import '../../routing_constants.dart';
+import 'package:eco_commerce_app/globals.dart' as globals;
+import 'package:flutter/services.dart';
+import 'package:line_awesome_icons/line_awesome_icons.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -14,7 +17,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     orders = new List();
     getOrders();
@@ -23,6 +25,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       resizeToAvoidBottomPadding: false,
       body: Stack(
         children: <Widget>[
@@ -31,14 +34,48 @@ class _ProfileScreenState extends State<ProfileScreen> {
               SliverAppBar(
                   stretch: true,
                   backgroundColor: Colors.white,
-                  automaticallyImplyLeading: true,
                   excludeHeaderSemantics: true,
-                  leading: new Container(),
+                  leading: IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    color: Colors.black,
+                    icon: Icon(LineAwesomeIcons.arrow_left),
+                  ),
+                  actions: <Widget>[
+                    IconButton(
+                      onPressed: () {
+                        print("Wishlist");
+                      },
+                      color: Colors.black,
+                      icon: Icon(LineAwesomeIcons.bookmark),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                        removeValue('jwt');
+                        removeValue('id');
+                        removeValue('confirmed');
+                        removeValue('blocked');
+                        removeValue('username');
+                        removeValue('email');
+                        removeValue('organisation');
+                        removeValue('orgemail');
+                        removeValue('phone');
+                        removeValue('createdAt');
+                        Navigator.pushReplacementNamed(context, LoginRoute);
+                      },
+                      color: Colors.black,
+                      icon: Icon(LineAwesomeIcons.sign_out),
+                    ),
+                  ],
                   pinned: true,
                   floating: false,
                   elevation: 0,
-                  expandedHeight: MediaQuery.of(context).size.height * (3 / 5),
+                  expandedHeight: MediaQuery.of(context).size.height * (2 / 5),
                   flexibleSpace: FlexibleSpaceBar(
+                    collapseMode: CollapseMode.pin,
+                    titlePadding: EdgeInsets.fromLTRB(60, 0, 0, 13),
                     background: ProfileFlexibleAppBar(),
                     title: Container(
                         padding: EdgeInsets.only(left: 0),
@@ -46,64 +83,65 @@ class _ProfileScreenState extends State<ProfileScreen> {
                           "Your Orders",
                           style: TextStyle(
                               fontFamily: "Poppins",
-                              fontSize: 15,
+                              fontSize: 20,
                               color: Color(0xff464646)),
                         )),
                   )),
               SliverList(delegate: SliverChildListDelegate(orders)),
             ],
           ),
-          Positioned(
-              left: MediaQuery.of(context).size.width / 2 - 120,
-              right: MediaQuery.of(context).size.width / 2 - 120,
-              bottom: 100,
-              child: Container(
-                height: 40,
-                child: RaisedButton(
-                  onPressed: () {
-                    print("WishList");
-                  },
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Text(
-                    "Your WishList",
-                    style: TextStyle(
-                        fontFamily: "Poppins",
-                        fontSize: 24,
-                        color: Colors.white),
-                  ),
-                  color: Color(0xff004445),
-                ),
-              )),
-          Positioned(
-              left: MediaQuery.of(context).size.width / 2 - 90,
-              right: MediaQuery.of(context).size.width / 2 - 90,
-              bottom: 20,
-              child: Container(
-                height: 40,
-                child: RaisedButton(
-                  onPressed: () {
-                    print("Sign Out");
-                  },
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Text(
-                    "SIGN OUT",
-                    style: TextStyle(
-                        fontFamily: "Poppins",
-                        fontSize: 24,
-                        color: Colors.white),
-                  ),
-                  color: Color(0xff004445),
-                ),
-              )),
+          // Positioned(
+          //     left: MediaQuery.of(context).size.width / 2 - 120,
+          //     right: MediaQuery.of(context).size.width / 2 - 120,
+          //     bottom: 100,
+          //     child: Container(
+          //       height: 40,
+          //       child: RaisedButton(
+          //         onPressed: () {
+          //           print("WishList");
+          //         },
+          //         shape: RoundedRectangleBorder(
+          //             borderRadius: BorderRadius.circular(10)),
+          //         child: Text(
+          //           "Your WishList",
+          //           style: TextStyle(
+          //               fontFamily: "Poppins",
+          //               fontSize: 24,
+          //               color: Colors.white),
+          //         ),
+          //         color: Color(0xff004445),
+          //       ),
+          //     )),
+          // Positioned(
+          //     left: MediaQuery.of(context).size.width / 2 - 90,
+          //     right: MediaQuery.of(context).size.width / 2 - 90,
+          //     bottom: 20,
+          //     child: Container(
+          //       height: 40,
+          //       child: RaisedButton(
+          //         onPressed: () {
+          //           print("Sign Out");
+          //         },
+          //         shape: RoundedRectangleBorder(
+          //             borderRadius: BorderRadius.circular(10)),
+          //         child: Text(
+          //           "SIGN OUT",
+          //           style: TextStyle(
+          //               fontFamily: "Poppins",
+          //               fontSize: 24,
+          //               color: Colors.white),
+          //         ),
+          //         color: Color(0xff004445),
+          //       ),
+          //     )),
         ],
       ),
     );
   }
 
   void getOrders() {
-    //TODO:: getActual Orders
+    orders.add(ProductListTile());
+    orders.add(ProductListTile());
     orders.add(ProductListTile());
     orders.add(ProductListTile());
     orders.add(ProductListTile());
@@ -121,26 +159,9 @@ class ProfileFlexibleAppBar extends StatefulWidget {
 }
 
 class _ProfileFlexibleAppBarState extends State<ProfileFlexibleAppBar> {
-  String userUsername;
-  String userEmail;
-  String userContact;
-  String org;
-  String location;
-
-  //TODO:: If It's Text Widget remove these
-  TextEditingController organisationController = TextEditingController();
-  TextEditingController locationController = TextEditingController();
-
   @override
   void initState() {
     super.initState();
-
-    //TODO :: get actual values
-    userUsername = "Pourav Raj";
-    userEmail = "pourav.raj@xyz.com";
-    userContact = "+91 9424XXXX01";
-    org = "XYZ Technologies Pvt. Ltd";
-    location = "New Delhi";
   }
 
   @override
@@ -158,9 +179,9 @@ class _ProfileFlexibleAppBarState extends State<ProfileFlexibleAppBar> {
                 children: <Widget>[
                   Container(
                       margin: EdgeInsets.only(
-                          left: 20, top: 20, right: 50, bottom: 4),
+                          left: 20, top: 0, right: 50, bottom: 4),
                       child: Text(
-                        userUsername,
+                        globals.currentUser.username,
                         style: TextStyle(
                             fontFamily: "Poppins",
                             fontSize: 40,
@@ -169,94 +190,84 @@ class _ProfileFlexibleAppBarState extends State<ProfileFlexibleAppBar> {
                   Container(
                       margin: EdgeInsets.only(left: 20),
                       child: Text(
-                        userEmail,
+                        globals.currentUser.email,
                         style: TextStyle(
                             fontFamily: "Poppins",
-                            fontSize: 28,
+                            fontSize: 24,
                             color: Color(0xff464646)),
                       )),
                   Container(
                       margin: EdgeInsets.only(left: 20),
                       child: Text(
-                        userContact,
+                        globals.currentUser.phone,
                         style: TextStyle(
                             fontFamily: "Poppins",
-                            fontSize: 28,
+                            fontSize: 24,
+                            color: Color(0xff464646)),
+                      )),
+                  Container(
+                      margin: EdgeInsets.only(left: 20),
+                      child: Text(
+                        globals.currentUser.organisation,
+                        style: TextStyle(
+                            fontFamily: "Poppins",
+                            fontSize: 24,
+                            color: Color(0xff464646)),
+                      )),
+                  Container(
+                      margin: EdgeInsets.only(left: 20),
+                      child: Text(
+                        globals.currentUser.orgemail,
+                        style: TextStyle(
+                            fontFamily: "Poppins",
+                            fontSize: 24,
                             color: Color(0xff464646)),
                       )),
                 ],
               ),
-              Container(
-                margin: EdgeInsets.all(20),
-                height: 100,
-                width: 100,
-                child: CircleAvatar(
-                  //TODO:: GET IMAGE
-                  child: Image.asset('assets/images/preSignUpSlider1.png'),
-                ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 0),
+                    child: Container(
+                      margin: EdgeInsets.all(20),
+                      height: 100,
+                      width: 100,
+                      child: CircleAvatar(
+                        backgroundColor: Color(0xFFC4C4C4),
+                        child: Text(
+                          globals.getInitials(globals.currentUser.username),
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              color: Color(0xFFE0E0E0),
+                              fontSize: 30),
+                        ),
+                      ),
+                    ),
+                  ),
+                  FlatButton(
+                    padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    onPressed: () {
+                      HapticFeedback.vibrate();
+                      print("Edit");
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      child: Text(
+                        "Edit",
+                        style: TextStyle(
+                            fontFamily: "Poppins",
+                            fontSize: 24,
+                            color: Color(0xff464646)),
+                      ),
+                    ),
+                  ),
+                ],
               )
             ],
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 15, right: 15),
-            child: ListTile(
-              title: Container(
-                  margin: EdgeInsets.only(bottom: 5),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Organisation",
-                        style: TextStyle(fontSize: 24),
-                      ),
-                      InkWell(
-                          onTap: () {
-                            print("Add New");
-                            Navigator.pushReplacementNamed(
-                                context, UserOptionalRoute);
-                          },
-                          child: Text(
-                            "Add+",
-                            style: TextStyle(fontSize: 20),
-                          ))
-                    ],
-                  )),
-              subtitle: new TextField(
-                controller: organisationController,
-                decoration: new InputDecoration(
-                  hintText: org,
-                  border: new OutlineInputBorder(
-                      borderSide:
-                          new BorderSide(width: 2, color: Colors.black)),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 15, right: 15),
-            child: ListTile(
-              title: Container(
-                  margin: EdgeInsets.only(bottom: 5),
-                  child: Text(
-                    "Location",
-                    style: TextStyle(fontSize: 24),
-                  )),
-              subtitle: new TextField(
-                controller: locationController,
-                decoration: new InputDecoration(
-                  hintText: location,
-                  border: new OutlineInputBorder(
-                      borderSide:
-                          new BorderSide(width: 2, color: Colors.black)),
-                ),
-              ),
-            ),
           ),
         ],
       ),
