1d1f00171741cbfb7fd42816a886851f5d2a2f08
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-06-12 17:44:02 +0530

    changes

--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,5 @@
 import 'package:eco_commerce_app/routing_constants.dart';
+import 'package:eco_commerce_app/ui/pages/about.dart';
 import 'package:eco_commerce_app/ui/pages/feedScreen.dart';
 import 'package:eco_commerce_app/ui/pages/loginScreen.dart';
 import 'package:eco_commerce_app/ui/pages/preSignUpSlider.dart';
@@ -19,6 +20,8 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => FeedScreen());
     case SearchRoute:
       return MaterialPageRoute(builder: (context) => SearchScreen());
+    case TnCRoute:
+      return MaterialPageRoute(builder: (context) => TermsConditions());
     default:
       return MaterialPageRoute(
           builder: (context) => UndefinedScreen(
--- lib/routing_constants.dart
+++ lib/routing_constants.dart
@@ -3,4 +3,4 @@ const String LoginRoute = '/login';
 const String RegisterRoute = '/register';
 const String FeedRoute = '/feed';
 const String SearchRoute = '/search';
-
+const String TnCRoute = '/terms';
