55b2d3da4018e5374b23d958d9a7e416a5cafa15
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-06-12 14:32:40 +0530

    ui changes

--- lib/ui/pages/categoryList.dart
+++ lib/ui/pages/categoryList.dart
@@ -1,78 +1,60 @@
+import 'package:eco_commerce_app/ui/widgets/headerText.dart';
+import 'package:eco_commerce_app/ui/widgets/mainDrawer.dart';
+import 'package:eco_commerce_app/ui/widgets/sectionHeader.dart';
 import 'package:flutter/material.dart';
 import 'package:eco_commerce_app/ui/widgets/productListTile.dart';
 import 'package:line_awesome_icons/line_awesome_icons.dart';
 
-class CategoryList extends StatefulWidget{
+class CategoryList extends StatefulWidget {
   @override
   _CategoryListState createState() => _CategoryListState();
 }
 
 class _CategoryListState extends State<CategoryList> {
-  
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      top: true,
       child: Scaffold(
         key: _scaffoldKey,
         appBar: AppBar(
-            elevation: 0,
-            backgroundColor: Colors.white,
-            brightness: Brightness.light,
-            leading: IconButton(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          brightness: Brightness.light,
+          leading: IconButton(
+            onPressed: () {
+              print("Nav Drawer");
+              _scaffoldKey.currentState.openDrawer();
+            },
+            color: Colors.black,
+            icon: Icon(LineAwesomeIcons.navicon),
+          ),
+          actions: <Widget>[
+            IconButton(
               onPressed: () {
-                print("Nav Drawer");
-                _scaffoldKey.currentState.openDrawer();
+                print("Search");
               },
               color: Colors.black,
-              icon: Icon(LineAwesomeIcons.navicon),
+              icon: Icon(LineAwesomeIcons.search),
             ),
-            actions: <Widget>[
-              IconButton(
-                onPressed: () {
-                  print("Search");
-                },
-                color: Colors.black,
-                icon: Icon(LineAwesomeIcons.search),
-              ),
-              IconButton(
-                onPressed: () {
-                  print("Bookmark");
-                },
-                color: Colors.black,
-                icon: Icon(LineAwesomeIcons.bookmark),
-              )
-            ],
-          ),
-
-        body: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-          ),
-          
-          child: ListView(
-            scrollDirection: Axis.vertical,
+            IconButton(
+              onPressed: () {
+                print("Bookmark");
+              },
+              color: Colors.black,
+              icon: Icon(LineAwesomeIcons.bookmark),
+            )
+          ],
+        ),
+        backgroundColor: Color(0xFFFFFFFF),
+        drawer: MainDrawer(),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              Text(
-                ' Pen',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontSize: 24,
-                 color: Colors.black,
-
-                ), 
-              ),
-              ProductListTile(),
-              ProductListTile(),
-              ProductListTile(),
-              ProductListTile(),
-              ProductListTile(),
-              ProductListTile(),
-              ProductListTile(),
-              ProductListTile(),
+              SectionHeader(text: 'Pens'),
               ProductListTile(),
               ProductListTile(),
               ProductListTile(),
