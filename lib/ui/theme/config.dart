9fc52ce83cd4ff535600ec66b72664cc8a5043a2
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-06-16 02:10:42 +0530

    Added theme data

---
+++ lib/ui/theme/config.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class App {
+  BuildContext _context;
+  double _height;
+  double _width;
+  double _heightPadding;
+  double _widthPadding;
+
+  App(_context) {
+    this._context = _context;
+    MediaQueryData _queryData = MediaQuery.of(this._context);
+    _height = _queryData.size.height / 100.0;
+    _width = _queryData.size.width / 100.0;
+    _heightPadding = _height -
+        ((_queryData.padding.top + _queryData.padding.bottom) / 100.0);
+    _widthPadding =
+        _width - (_queryData.padding.left + _queryData.padding.right) / 100.0;
+  }
+
+  double appHeight(double v) {
+    return _height * v;
+  }
+
+  double appWidth(double v) {
+    return _width * v;
+  }
+
+  double appVerticalPadding(double v) {
+    return _heightPadding * v;
+  }
+
+  double appHorizontalPadding(double v) {
+    return _widthPadding * v;
+  }
+}
+
+class Colors {
+  Color _mainColor = Color(0xFF1C1C1C);
+  Color _secondColor = Color(0xFF004445);
+  Color _accentColor = Color(0xFF96EFA6);
+  LinearGradient deepSpace = LinearGradient(
+    colors: [Color(0xFF4CA1AF), Color(0xFF2C3E50)],
+    begin: Alignment.topRight,
+    end: Alignment.bottomLeft,
+  );
+  LinearGradient peachy = LinearGradient(
+    colors: [Color(0xFFFFB382), Color(0xFFF07590)],
+    begin: Alignment.topRight,
+    end: Alignment.bottomLeft,
+  );
+  LinearGradient nebula = LinearGradient(
+    colors: [Color(0xFFA1A3FF), Color(0xFF6D63EF)],
+    begin: Alignment.topRight,
+    end: Alignment.bottomLeft,
+  );
+  LinearGradient mildSea = LinearGradient(
+    colors: [Color(0xFF96EFA6), Color(0xFF26A6B5)],
+    begin: Alignment.topRight,
+    end: Alignment.bottomLeft,
+  );
+  LinearGradient purplin = LinearGradient(
+    colors: [Color(0xFFA044FF), Color(0xFF6A3093)],
+    begin: Alignment.topRight,
+    end: Alignment.bottomLeft,
+  );
+
+  LinearGradient easyMed = LinearGradient(
+    colors: [Color(0xFF45B649), Color(0xFFDCE35B)],
+    begin: Alignment.topRight,
+    end: Alignment.bottomLeft,
+  );
+
+  Color mainColor(double opacity) {
+    return this._mainColor.withOpacity(opacity);
+  }
+
+  Color secondColor(double opacity) {
+    return this._secondColor.withOpacity(opacity);
+  }
+
+  Color accentColor(double opacity) {
+    return this._accentColor.withOpacity(opacity);
+  }
+}
---
+++ lib/ui/theme/theme.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:eco_commerce_app/ui/theme/config.dart' as config;
+
+var kDefaultTheme = ThemeData(
+  primaryColor: Colors.white,
+  brightness: Brightness.light,
+  accentColor: config.Colors().accentColor(1),
+  focusColor: config.Colors().secondColor(1),
+  hintColor: config.Colors().accentColor(1),
+  accentTextTheme: TextTheme(headline6: TextStyle(fontFamily: "Poppins")),
+  textTheme: TextTheme(
+    button: TextStyle(
+      fontFamily: 'Poppins',
+      fontSize: 20,
+      fontWeight: FontWeight.w600,
+      color: Color(0xFFFFFFFF),
+    ),
+    headline5: TextStyle(
+      fontSize: 20.0,
+      color: config.Colors().mainColor(1),
+      fontFamily: "Poppins",
+    ),
+    headline4: TextStyle(
+      fontFamily: 'Roboto',
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: config.Colors().secondColor(1),
+    ),
+    headline3: TextStyle(
+      fontFamily: 'Poppins',
+      fontSize: 22,
+      fontWeight: FontWeight.bold,
+      color: config.Colors().accentColor(1),
+    ),
+    headline2: TextStyle(
+      fontSize: 25,
+      fontFamily: "Poppins",
+      fontWeight: FontWeight.w700,
+      color: config.Colors().mainColor(1),
+    ),
+    headline1: TextStyle(
+      fontFamily: 'Poppins',
+      color: Colors.white,
+      fontSize: 50,
+      fontWeight: FontWeight.w600,
+    ),
+    subtitle1: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w900,
+      color: config.Colors().secondColor(1),
+      fontFamily: "Roboto",
+    ),
+    headline6: TextStyle(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w600,
+      color: config.Colors().mainColor(1),
+      fontFamily: "Poppins",
+    ),
+    bodyText2: TextStyle(
+        fontFamily: 'Poppins',
+        color: config.Colors().mainColor(1),
+        fontSize: 16),
+    bodyText1: TextStyle(
+      fontFamily: 'Poppins',
+      fontSize: 36,
+      fontWeight: FontWeight.w400,
+      color: config.Colors().accentColor(1),
+    ),
+    caption: TextStyle(
+      fontFamily: 'Roboto',
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      color: config.Colors().accentColor(1),
+    ),
+  ),
+);
