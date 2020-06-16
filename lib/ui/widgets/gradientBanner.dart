4d848a78b258927ce56680bb3b882e51aff6bcbd
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-06-17 01:39:45 +0530

    Added gradientbanner widget

---
+++ lib/ui/widgets/gradientBanner.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class GradientBanner extends StatelessWidget {
+  const GradientBanner({
+    Key key,
+    @required this.gradient,
+    @required this.message,
+  }) : super(key: key);
+
+  final LinearGradient gradient;
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: gradient,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 50, 30, 50),
+        child: Text(
+          message,
+          style: Theme.of(context)
+              .textTheme
+              .headline3
+              .copyWith(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
