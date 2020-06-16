643411752e15ed25e1c2a0faf665ea3d282b9a4d
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-06-17 02:50:38 +0530

    fixed popup UI

--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 - [x] Fluttertoast from snackbar
 - [x] make phone required
 - [x] Submit button login, to check mark when successful
-- [ ] Remove the clutter from sign up (google vs Free email,second - org, orgmail, phone|name,email,phone,third-org,orgmail,fourth-pswd,cnfm pswd)
-- [x] Gamify sign up screen (Progress bar, Cross mark to PresignUp,)
+- [x] Remove the clutter from sign up (google vs Free email,second - org, orgmail, phone|name,email,phone,third-org,orgmail,fourth-pswd,cnfm pswd)
+- [ ] Gamify sign up screen (Progress bar, Cross mark to PresignUp,)
 - [x] UI update PreSignUp Intro Slider
 - [x] Snackbars to popup with GIFs
 - [x] Search should be working
@@ -24,11 +24,11 @@
 - [x] Padding issue in drawer
 - [x] Remove social media icons, shift to about us
 - [x] remove share with friends
-- [ ] make help/FAQ screens
+- [x] make help/FAQ screens
 - [x] tnc, help, faq, rate us && settings, about us && home, trending products, shop by category, all products, wishlist, your profile
 - [ ] Category pages should be filled with grids as much products as can be filled in 1 screen, with prices / name only
-- [ ] use linear loader below appbar
-- [ ] category name should be appbar title
+- [x] use linear loader below appbar
+- [x] category name should be appbar title
 - [ ] category should have a slider after four products
 - [ ] use tooltips allover everywhere
 - [x] product page UI update (name should be small, pictures should be fully wide, show carousel indicators)
@@ -37,7 +37,7 @@
 - [x] product page UI update (description and features with different widgets, increase spacing over all, add badges in UI)
 - [ ] order now working (db update, email update)
 - [x] email on order successful
-- [ ] categories should be appbar title
+- [x] categories should be appbar title
 - [ ] search wishlist only on home, appbar cleaning
 - [x] back button product page appbar
 - [x] replace snackbars with toasts in google sign in
--- lib/ui/widgets/popUp.dart
+++ lib/ui/widgets/popUp.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:eco_commerce_app/ui/theme/config.dart' as config;
 
-Future popUpAlertDialog(
-    {BuildContext context,
-    String text,
-    Function func1,
-    Function func2,
-    bool twoButtons,
-    String button1text,
-    String button2text}) {
+Future popUpAlertDialog({
+  BuildContext context,
+  String text,
+  Function func1,
+  Function func2,
+  bool twoButtons,
+  String button1text,
+  String button2text,
+}) {
   return showDialog(
       context: context,
       barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          backgroundColor: Color(0xFFF07590),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
           content: Text(
             text,
-            style: TextStyle(
-              fontFamily: 'Poppins',
-              fontSize: 16,
-            ),
+            style: Theme.of(context)
+                .textTheme
+                .headline6
+                .copyWith(color: Theme.of(context).primaryColor),
           ),
           actions: [
             twoButtons == true
@@ -28,94 +32,149 @@ Future popUpAlertDialog(
                       Padding(
                         padding: const EdgeInsets.all(12),
                         child: Container(
-                          height: MediaQuery.of(context).size.height * 0.051,
-                          width: MediaQuery.of(context).size.width * 0.3,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
+                            height: MediaQuery.of(context).size.height * 0.051,
+                            width: MediaQuery.of(context).size.width * 0.3,
+                            decoration: BoxDecoration(
                               boxShadow: [
                                 BoxShadow(
-                                    offset: Offset(0, 3),
-                                    blurRadius: 10,
-                                    color: Color(0xFF004445).withOpacity(0.2))
-                              ]),
-                          child: RaisedButton(
-                            color: Color(0xFFFFFFFF),
-                            elevation: 0.0,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(32.0),
+                                    color: Colors.transparent,
+                                    blurRadius: 16,
+                                    offset: Offset(0, 4)),
+                              ],
+                              gradient: LinearGradient(
+                                colors: [
+                                  Color(0xFFFFFFFF).withOpacity(0.2),
+                                  Color(0xFFFFFFFF).withOpacity(0.2)
+                                ],
+                                begin: Alignment.topRight,
+                                end: Alignment.bottomLeft,
+                              ),
+                              borderRadius: BorderRadius.circular(500),
                             ),
-                            onPressed: func1 == null ? () {} : func1,
-                            child: Text(
-                              button1text == null ? "OK" : button1text,
-                              style: TextStyle(
-                                  fontFamily: 'Montserrat',
-                                  color: Color(0xFF004445),
-                                  fontSize: 16),
+                            child: FlatButton(
+                              padding: EdgeInsets.all(0),
+                              colorBrightness: Brightness.dark,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(500)),
+                              color: Colors.transparent,
+                              onPressed: func1 == null ? () {} : func1,
+                              child: Text(
+                                button1text == null ? "OK" : button1text,
+                                style: Theme.of(context).textTheme.button,
+                              ),
+                            )
+                            //  RaisedButton(
+                            //   color: Color(0xFFFFFFFF),
+                            //   elevation: 0.0,
+                            //   shape: RoundedRectangleBorder(
+                            //     borderRadius: BorderRadius.circular(32.0),
+                            //   ),
+                            //   onPressed: func1 == null ? () {} : func1,
+                            // child: Text(
+                            //   button1text == null ? "OK" : button1text,
+                            //   style: Theme.of(context)
+                            //       .textTheme
+                            //       .button
+                            //       .copyWith(
+                            //           color: config.Colors().secondColor(1)),
+                            // ),
+                            // ),
                             ),
-                          ),
-                        ),
                       ),
                       Padding(
                         padding: const EdgeInsets.all(12),
                         child: Container(
-                          height: MediaQuery.of(context).size.height * 0.051,
-                          width: MediaQuery.of(context).size.width * 0.3,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
+                            height: MediaQuery.of(context).size.height * 0.051,
+                            width: MediaQuery.of(context).size.width * 0.3,
+                            decoration: BoxDecoration(
                               boxShadow: [
                                 BoxShadow(
-                                    offset: Offset(0, 3),
-                                    blurRadius: 10,
-                                    color: Color(0xFF004445).withOpacity(0.5))
-                              ]),
-                          child: RaisedButton(
-                            color: Color(0xFF004445),
-                            elevation: 0.0,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(32.0),
+                                    color: Color(0xFF76ED92).withOpacity(0.4),
+                                    blurRadius: 16,
+                                    offset: Offset(0, 4)),
+                              ],
+                              gradient: LinearGradient(
+                                colors: [Color(0xFF96EFA6), Color(0xFF26A6B5)],
+                                begin: Alignment.topRight,
+                                end: Alignment.bottomLeft,
+                              ),
+                              borderRadius: BorderRadius.circular(500),
                             ),
-                            onPressed: func2 == null ? () {} : func2,
-                            child: Text(
-                              button2text == null ? "OK" : button2text,
-                              style: TextStyle(
-                                  fontFamily: 'Montserrat',
-                                  color: Colors.white,
-                                  fontSize: 16),
+                            child: FlatButton(
+                              padding: EdgeInsets.all(0),
+                              colorBrightness: Brightness.dark,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(500)),
+                              color: Colors.transparent,
+                              onPressed: func2 == null ? () {} : func2,
+                              child: Text(
+                                button2text == null ? "OK" : button2text,
+                                style: Theme.of(context).textTheme.button,
+                              ),
+                            )
+                            // RaisedButton(
+                            //   color: Color(0xFF004445),
+                            //   elevation: 0.0,
+                            //   shape: RoundedRectangleBorder(
+                            //     borderRadius: BorderRadius.circular(32.0),
+                            //   ),
+                            //   onPressed: func2 == null ? () {} : func2,
+                            // child: Text(
+                            //   button2text == null ? "OK" : button2text,
+                            //   style: Theme.of(context).textTheme.button,
+                            // ),
+                            // ),
                             ),
-                          ),
-                        ),
                       ),
                     ],
                   )
                 : Padding(
                     padding: const EdgeInsets.all(16),
                     child: Container(
-                      height: MediaQuery.of(context).size.height * 0.051,
-                      width: MediaQuery.of(context).size.width * 0.6,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
+                        height: MediaQuery.of(context).size.height * 0.051,
+                        width: MediaQuery.of(context).size.width * 0.6,
+                        decoration: BoxDecoration(
                           boxShadow: [
                             BoxShadow(
-                                offset: Offset(0, 3),
-                                blurRadius: 10,
-                                color: Color(0xFF004445).withOpacity(0.5))
-                          ]),
-                      child: RaisedButton(
-                        color: Color(0xFF004445),
-                        elevation: 0.0,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(32.0),
+                                color: Colors.transparent,
+                                blurRadius: 16,
+                                offset: Offset(0, 4)),
+                          ],
+                          gradient: LinearGradient(
+                            colors: [
+                              Color(0xFFFFFFFF).withOpacity(0.2),
+                              Color(0xFFFFFFFF).withOpacity(0.2)
+                            ],
+                            begin: Alignment.topRight,
+                            end: Alignment.bottomLeft,
+                          ),
+                          borderRadius: BorderRadius.circular(500),
                         ),
-                        onPressed: func1 == null ? () {} : func1,
-                        child: Text(
-                          button1text == null ? "OK" : button1text,
-                          style: TextStyle(
-                              fontFamily: 'Montserrat',
-                              color: Colors.white,
-                              fontSize: 16),
+                        child: FlatButton(
+                          padding: EdgeInsets.all(0),
+                          colorBrightness: Brightness.dark,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(500)),
+                          color: Colors.transparent,
+                          onPressed: func1 == null ? () {} : func1,
+                          child: Text(
+                            button1text == null ? "OK" : button1text,
+                            style: Theme.of(context).textTheme.button,
+                          ),
+                        )
+                        // RaisedButton(
+                        //   color: Color(0xFF004445),
+                        //   elevation: 0.0,
+                        //   shape: RoundedRectangleBorder(
+                        //     borderRadius: BorderRadius.circular(32.0),
+                        //   ),
+                        //   onPressed: func1 == null ? () {} : func1,
+                        // child: Text(
+                        //   button1text == null ? "OK" : button1text,
+                        //   style: Theme.of(context).textTheme.button,
+                        // ),
+                        // ),
                         ),
-                      ),
-                    ),
                   ),
           ],
         );
