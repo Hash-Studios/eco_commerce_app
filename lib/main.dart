79326124e5757570e1db9dd7afa85d07d6ad95c7
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-06-18 02:49:07 +0530

    firebase analytics added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:eco_commerce_app/core/provider/user.dart';
+import 'package:eco_commerce_app/core/services/analytics_service.dart';
 import 'package:eco_commerce_app/routing_constants.dart';
 import 'package:eco_commerce_app/ui/pages/undefinedScreen.dart';
 import 'package:eco_commerce_app/ui/theme/theme.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 import 'package:eco_commerce_app/router.dart' as router;
 import 'package:provider/provider.dart';
@@ -9,6 +11,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'routing_constants.dart';
 
 SharedPreferences prefs;
+final FirebaseAnalyticsObserver observer =
+    FirebaseAnalyticsObserver(analytics: analytics);
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   prefs = await SharedPreferences.getInstance();
@@ -34,6 +38,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorObservers: <NavigatorObserver>[observer],
       onGenerateRoute: router.generateRoute,
       debugShowCheckedModeBanner: false,
       onUnknownRoute: (settings) => MaterialPageRoute(
